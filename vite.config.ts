f1ee2d632b919515a293985eea70949d5aca798b
sutirtha <webdev1@mobotics.in>
2024-08-30 15:15:12 +0530

    update

--- vite.config.ts
+++ vite.config.ts
@@ -19,9 +19,8 @@ export default defineConfig({
             '@': path.join(__dirname, 'src'),
         },
     },
-  },
-  base: '/ihrc/',
-  build: {
-      outDir: 'dist',
-  },
-);
+    base: '/ihrc/',
+    build: {
+        outDir: 'dist',
+    },
+})
