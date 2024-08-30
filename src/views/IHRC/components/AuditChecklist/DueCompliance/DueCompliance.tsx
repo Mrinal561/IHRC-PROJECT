18f657315dc06bd97fe1f1c7940d0bf9b81649f0
Sumit-mobotics <Sumit@mobotics.in>
2024-08-30 11:01:11 +0530

    Due Compliance modified

--- src/views/IHRC/components/AuditChecklist/DueCompliance/DueCompliance.tsx
+++ src/views/IHRC/components/AuditChecklist/DueCompliance/DueCompliance.tsx
@@ -6,7 +6,7 @@ const DueCompliance = () => {
   return (
     <AdaptableCard className="h-full" bodyClass="h-full">
             <div className="lg:flex items-center justify-between mb-8">
-                <h3 className="mb-4 lg:mb-0">Due Compliance</h3>
+                <h3 className="mb-4 lg:mb-0">Due & Upcoming Compliance</h3>
                 <DueComplianceTableTool />
             </div>
                 <DueComplianceTable />
