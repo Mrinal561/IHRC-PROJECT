257e99973b47afbeff83d0ebd19d6841f40c128b
Rajdip Dutta <imrajdipdutta@gmail.com>
2024-08-30 17:23:05 +0530

    Changes for setup in server

--- src/configs/app.config.ts
+++ src/configs/app.config.ts
@@ -8,7 +8,7 @@ export type AppConfig = {
 }
 
 const appConfig: AppConfig = {
-    apiPrefix: '/api',
+    apiPrefix: '/ihrc/api',
     authenticatedEntryPath: '/home',
     unAuthenticatedEntryPath: '/sign-in',
     tourPath: '/',
