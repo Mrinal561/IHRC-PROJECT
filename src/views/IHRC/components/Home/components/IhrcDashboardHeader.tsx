c52a44c1a188b9fdc18033179778ed2db944a2cf
Mrinal561 <mrinal@mobotics.in>
2024-09-03 21:33:53 +0530

    changes

--- src/views/IHRC/components/Home/components/IhrcDashboardHeader.tsx
+++ src/views/IHRC/components/Home/components/IhrcDashboardHeader.tsx
@@ -15,7 +15,7 @@ const IhrcDashboardHeader = () => {
         <div className="lg:flex items-center justify-between mb-4 gap-3">
             <div className="mb-4 lg:mb-0">
                 <h3>Overview</h3>
-                <p>View your current reports & summary</p>
+                <p>View your company's compliance statistics</p>
             </div>
             <div className="flex flex-col lg:flex-row lg:items-center gap-3">
                 <DatePickerRange
