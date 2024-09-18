26e9773910e84bce42661186659848ced590b533
Sumit-mobotics <Sumit@mobotics.in>
2024-09-18 12:57:41 +0530

    commited

--- src/views/IHRC/components/RemittanceTracker/ESITracker/components/ESITrackerEditDialog.tsx
+++ src/views/IHRC/components/RemittanceTracker/ESITracker/components/ESITrackerEditDialog.tsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react';
 import { Dialog, Button, DatePicker, toast, Notification } from '@/components/ui';
 import OutlinedInput from '@/components/ui/OutlinedInput';
 import { ESITrackerData } from './ESITrackerTable';
+import OutlinedSelect from '@/components/ui/Outlined';
 
 interface ESITrackerEditDialogProps {
   isOpen: boolean;
@@ -50,97 +51,169 @@ const ESITrackerEditDialog: React.FC<ESITrackerEditDialogProps> = ({
     )
   }
 
+  const challanTypeOptions = [
+    { value: 'Main Challan', label: 'Main Challan' },
+    { value: 'Supplementary Challan', label: 'Supplementary Challan' },
+  ];
+
   return (
     <Dialog
       isOpen={isOpen}
       onClose={onClose}
       onRequestClose={onClose}
       width={800}
+      height={600}
     >
       <h5 className="mb-4">Edit ESI Tracker</h5>
-      <div className="grid grid-cols-2 gap-4">
-        <OutlinedInput
-          label="No. of Employees"
-          value={editedData.noOfEmployees.toString()}
-          onChange={(value) => handleChange('noOfEmployees', parseInt(value, 10))}
-        />
-        <OutlinedInput
-          label="ESI Gross Wages"
-          value={editedData.esiGrossWages.toString()}
-          onChange={(value) => handleChange('esiGrossWages', parseFloat(value))}
-        />
-      </div>
-      <div className="grid grid-cols-3 gap-4 mt-2">
-        <div className="flex flex-col ">
-          <label>Please select the Month</label>
-          <DatePicker
-            placeholder="Month"
-            value={new Date(editedData.month)}
-            onChange={(date) => handleDateChange('month', date)}
-          />
+      
+      <div className="p-4 space-y-4">
+        <div className='flex gap-4 items-center'>
+        <div className='flex flex-col gap-2 w-full'>
+            <label>Enter Number of Employees</label>
+            <div className='w-full'>
+              <OutlinedInput
+                label="No. of Employees"
+                value={editedData.noOfEmployees.toString()}
+                onChange={(value) => handleChange('noOfEmployees', parseInt(value, 10))}
+              />
+            </div>
+          </div>
+          <div className='flex flex-col gap-2 w-full'>
+            <label>Enter Challan No .</label>
+            <div className='w-full'>
+              <OutlinedInput
+                label="Challan No"
+                value={editedData.challanNo}
+                onChange={(value) => handleChange('challanNo', value)}
+              />
+            </div>
+          </div>
         </div>
-        <div className="flex flex-col ">
-          <label>Due Date</label>
-          <DatePicker
-            placeholder="Due Date"
-            value={new Date(editedData.dueDate)}
-            onChange={(date) => handleDateChange('dueDate', date)}
-          />
+
+        <div className='flex gap-8 items-center'>
+          <div className='flex flex-col gap-2'>
+            <label>Select Month</label>
+            <div className='w-[219px]'>
+              <DatePicker
+                placeholder="Month"
+                value={new Date(editedData.month)}
+                onChange={(date) => handleDateChange('month', date)}
+              />
+            </div>
+          </div>
+          <div className='flex flex-col gap-2'>
+            <label>Select Due Date</label>
+            <div className='w-[219px]'>
+              <DatePicker
+                placeholder="Due Date"
+                value={new Date(editedData.dueDate)}
+                onChange={(date) => handleDateChange('dueDate', date)}
+              />
+            </div>
+          </div>
+          <div className='flex flex-col gap-2'>
+            <label>Select Date of Payment</label>
+            <div className='w-[219px]'>
+              <DatePicker
+                placeholder="Amount Paid On"
+                value={new Date(editedData.amountPaidOn)}
+                onChange={(date) => handleDateChange('amountPaidOn', date)}
+              />
+            </div>
+          </div>
         </div>
-        <div className="flex flex-col">
-          <label>Amount Paid On</label>
-          <DatePicker
-            placeholder="Amount Paid On"
-            value={new Date(editedData.amountPaidOn)}
-            onChange={(date) => handleDateChange('amountPaidOn', date)}
-          />
+        <div className="flex gap-8 items-center">
+          <div className='flex flex-col gap-2'>
+            <label>Enter ESI Gross Wages</label>
+            <div className='w-[219px]'>
+              <OutlinedInput
+                label="ESI Gross Wages"
+                value={editedData.esiGrossWages.toString()}
+                onChange={(value) => handleChange('esiGrossWages', parseFloat(value))}
+              />
+            </div>
+          </div>
+          <div className='flex flex-col gap-2'>
+            <label>Enter EE ESI</label>
+            <div className='w-[219px]'>
+              <OutlinedInput
+                label="EE ESI"
+                value={editedData.eeESI.toString()}
+                onChange={(value) => handleChange('eeESI', parseFloat(value))}
+              />
+            </div>
+          </div>
+          <div className='flex flex-col gap-2'>
+            <label>ER ESI</label>
+            <div className='w-[219px]'>
+              <OutlinedInput
+                label="Total Amount As per Challan"
+                value={editedData.erESI.toString()}
+                onChange={(value) => handleChange('erESI', parseFloat(value))}
+              />
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="grid grid-cols-3 gap-4 mt-4">
-        <OutlinedInput
-          label="EE ESI"
-          value={editedData.eeESI.toString()}
-          onChange={(value) => handleChange('eeESI', parseFloat(value))}
-        />
-        <OutlinedInput
-          label="ER ESI"
-          value={editedData.erESI.toString()}
-          onChange={(value) => handleChange('erESI', parseFloat(value))}
-        />
-        <OutlinedInput
-          label="Total ESI"
-          value={editedData.totalESI.toString()}
-          onChange={(value) => handleChange('totalESI', parseFloat(value))}
-        />
-      </div>
-      <div className="grid grid-cols-3 gap-4 mt-4">
-        <OutlinedInput
-          label="Total Amount As per Challan"
-          value={editedData.totalAmountAsPerChallan.toString()}
-          onChange={(value) => handleChange('totalAmountAsPerChallan', parseFloat(value))}
-        />
-        <OutlinedInput
-          label="Difference in Amount"
-          value={editedData.differenceInAmount.toString()}
-          onChange={(value) => handleChange('differenceInAmount', parseFloat(value))}
-        />
-        <OutlinedInput
-          label="Delay"
-          value={editedData.delay}
-          onChange={(value) => handleChange('delay', value)}
-        />
-      </div>
-      <div className="grid grid-cols-2 gap-4 mt-4">
-        <OutlinedInput
-          label="Delay Reason"
-          value={editedData.delayReason}
-          onChange={(value) => handleChange('delayReason', value)}
-        />
-        <OutlinedInput
-          label="Challan No"
-          value={editedData.challanNo}
-          onChange={(value) => handleChange('challanNo', value)}
-        />
+
+        
+
+        <div className='flex gap-4 items-center'>
+          <div className='flex flex-col gap-2 w-full'>
+            <label>Select Type of Challan</label>
+            <div className='w-full'>
+              <OutlinedSelect
+                label="Type of Challan"
+                options={challanTypeOptions}
+                value={challanTypeOptions.find(option => option.value === editedData.typeOfChallan)}
+                onChange={(option) => handleChange('typeOfChallan', option?.value || '')}
+              />
+            </div>
+           
+          </div>
+          <div className='flex flex-col gap-2 w-full'>
+            <label>Difference In Amount</label>
+            <div className='w-full'>
+              <OutlinedInput
+                label="Difference In Amount"
+                value={editedData.differenceInAmount.toString()}
+                onChange={(value) => handleChange('differenceInAmount', parseFloat(value))}
+              />
+            </div>
+          </div>
+          <div className='flex flex-col gap-2 w-full'>
+          <label>Enter Total Amount</label>
+            <div className='w-[219px]'>
+              <OutlinedInput
+                label="Total Amount As per Challan"
+                value={editedData.totalAmountAsPerChallan.toString()}
+                onChange={(value) => handleChange('totalAmountAsPerChallan', parseFloat(value))}
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className='flex gap-4 items-center'>
+          <div className='flex flex-col gap-2 w-full'>
+            <label>Enter Delay</label>
+            <div className='w-full'>
+              <OutlinedInput
+                label="Delay"
+                value={editedData.delay}
+                onChange={(value) => handleChange('delay', value)}
+              />
+            </div>
+          </div>
+          <div className='flex flex-col gap-2 w-full'>
+            <label>Enter Delay Reason</label>
+            <div className='w-full'>
+              <OutlinedInput
+                label="Delay Reason"
+                value={editedData.delayReason}
+                onChange={(value) => handleChange('delayReason', value)}
+              />
+            </div>
+          </div>
+        </div>
       </div>
       <div className="flex justify-end mt-6">
         <Button variant="plain" onClick={onClose} className="mr-2">
