d09c0acd549f1233e3d5419135ffcf61c6b7a439
Mrinal81 <mrinal.anand9123@gmail.com>
2024-09-19 16:56:39 +0530

    updates

--- src/views/IHRC/components/RemittanceTracker/PTECTracker/PTECTracker.tsx
+++ src/views/IHRC/components/RemittanceTracker/PTECTracker/PTECTracker.tsx
@@ -13,7 +13,7 @@ const PTECTracker = () => {
 
   return (
     <AdaptableCard className="h-full" bodyClass="h-full">
-      <div className="flex flex-col lg:flex-row lg:items-center justify-between mb-10">
+      <div className="flex flex-wrap gap-6 items-center justify-between mb-6">
         <div className="mb-4 lg:mb-0">
           <h3 className="text-2xl font-bold">PT EC Tracker</h3>
         </div>
