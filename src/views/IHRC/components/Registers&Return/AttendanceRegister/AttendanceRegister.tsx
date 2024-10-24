a07013eb916b56e797e571dd7a78a2e79bfbfd7c
Sumit-mobotics <Sumit@mobotics.in>
2024-10-24 15:05:23 +0530

    salary register added

--- src/views/IHRC/components/Registers&Return/AttendanceRegister/AttendanceRegister.tsx
+++ src/views/IHRC/components/Registers&Return/AttendanceRegister/AttendanceRegister.tsx
@@ -1,8 +1,23 @@
+import { AdaptableCard } from '@/components/shared'
 import React from 'react'
+import AttendanceFilter from './components/AttendanceFilter'
 
 const AttendanceRegister = () => {
   return (
-    <div>Attendance Register</div>
+    <AdaptableCard className="h-full" bodyClass="h-full">
+            <div className="flex flex-row items-center justify-between mb-10">
+
+            <div className="">
+                    <h3 className="text-2xl font-bold">Attendance Register Input</h3>
+                    {/* <p className="text-gray-600">View your company's recommended compliance </p> */}
+            </div>
+            <AttendanceFilter/>
+            </div>
+            <div className='mb-8'>
+            {/* <Company /> */}
+            </div>
+                {/* <RecommendedTableContent /> */}
+        </AdaptableCard>
   )
 }
 
--- src/views/IHRC/components/Registers&Return/LeaveRegister/LeaveRegister.tsx
+++ src/views/IHRC/components/Registers&Return/LeaveRegister/LeaveRegister.tsx
@@ -1,8 +1,20 @@
+import { AdaptableCard } from '@/components/shared'
 import React from 'react'
+import LeaveFilter from './components/LeaveFilter'
 
 const LeaveRegister = () => {
   return (
-    <div>Leave Register</div>
+    <AdaptableCard className="h-full" bodyClass="h-full">
+            <div className="flex flex-row items-center justify-between mb-10">
+
+            <div className="">
+                    <h3 className="text-2xl font-bold">Leave Register Input</h3>
+                    {/* <p className="text-gray-600">View your company's recommended compliance </p> */}
+            </div>
+            <LeaveFilter/>
+            </div>
+                {/* <RecommendedTableContent /> */}
+        </AdaptableCard>
   )
 }
 
