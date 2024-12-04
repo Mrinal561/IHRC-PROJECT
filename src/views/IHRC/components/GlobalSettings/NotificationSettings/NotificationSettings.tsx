e66ae5f20ce0537856be1e670c158ca149b05a91
Mrinal81 <mrinal.anand9123@gmail.com>
2024-12-04 11:58:18 +0530

    still in development phase ui added

--- src/views/IHRC/components/GlobalSettings/NotificationSettings/NotificationSettings.tsx
+++ src/views/IHRC/components/GlobalSettings/NotificationSettings/NotificationSettings.tsx
@@ -1,16 +1,23 @@
 import React from 'react'
 import NotificationContent from './components/NotificationContent'
+import { HiOutlineViewGrid } from 'react-icons/hi'
 
 const NotificationSettings = () => {
   return (
-    <div>
+    <>
       <div className='mb-12 flex justify-between items-center'>
            <h3 className="text-2xl font-bold">Notification Settings</h3>
       </div>
 
-      <NotificationContent />
+      {/* <NotificationContent /> */}
+      <div className="flex flex-col items-center justify-center h-full text-gray-500 border rounded-xl">
+                <HiOutlineViewGrid className="w-12 h-12 mb-4 text-gray-300" />
+                <p className="text-center">
+        Still in Development Phase
+                </p>
+      </div>
 
-    </div>
+    </>
   )
 }
 
--- src/views/IHRC/components/GlobalSettings/StatusSettings/StatusSettings.tsx
+++ src/views/IHRC/components/GlobalSettings/StatusSettings/StatusSettings.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { HiOutlineViewGrid } from 'react-icons/hi'
 
 const StatusSettings = () => {
   return (
-    <div>StatusSettings</div>
+    <>
+       <div className='mb-12 flex justify-between items-center'>
+            <h3 className="text-2xl font-bold">Status Settings</h3>
+       </div>
+
+    <div className="flex flex-col items-center justify-center h-full text-gray-500 border rounded-xl">
+    <HiOutlineViewGrid className="w-12 h-12 mb-4 text-gray-300" />
+    <p className="text-center">
+Still in Development Phase
+    </p>
+</div>
+    </>
   )
 }
 
